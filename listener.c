54dac3f809b3833dcc8c551940a3a6550386bbf6
Earl <Earl@27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a>
2009-05-03 15:14:03 +0000

    git-svn-id: http://galileo.dmi.unict.it/svn/weborf/trunk@216 27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a

--- listener.c
+++ listener.c
@@ -351,7 +351,7 @@ int main(int argc, char * argv[]) {
             getpeername(s1, (struct sockaddr *)&farAddr, &farAddrL);
             inet_ntop(AF_INET6, &farAddr.sin6_addr, ip_addr, INET6_ADDRSTRLEN);
         }
-     }
+     
 #else
     while ((s1 = accept(s, (struct sockaddr *) &farAddr,(socklen_t *)&farAddrL)) != -1) {
 
@@ -360,7 +360,7 @@ int main(int argc, char * argv[]) {
             getpeername(s1, (struct sockaddr *)&farAddr,(socklen_t *) &farAddrL);
             inet_ntop(AF_INET, &farAddr.sin_addr, ip_addr, INET_ADDRSTRLEN);
         }
-     }
+     
 #endif
 
 
@@ -386,7 +386,7 @@ int main(int argc, char * argv[]) {
             }
         }
    
-
+    }
     return 0;
 
 }
