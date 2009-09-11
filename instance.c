6a051db4c6b514ed42214444b6638c0c4e2cba36
LtWorf <LtWorf@27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a>
2009-09-11 15:33:45 +0000

    changed condition, it caused partial resending of files
    git-svn-id: http://galileo.dmi.unict.it/svn/weborf/trunk@244 27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a

--- instance.c
+++ instance.c
@@ -602,8 +602,7 @@ send large files or not.
 If the file is larger, it will be sent using writeCompressedFile,
 see that function for details.
 */
-int writeFile(int sock,char * strfile,char *http_param) {
-
+int writeFile(int sock,char * strfile,char *http_param) {    
     int fp=open(strfile,O_RDONLY | O_LARGEFILE);
     if (fp<0) { //open returned an error
         return ERR_FILENOTFOUND;
@@ -644,8 +643,6 @@ int writeFile(int sock,char * strfile,char *http_param) {
         return ERR_NOMEM;//If no memory is available
     }
 
-
-
     off_t count=size;//Bytes to send to the client
 
 #ifdef __RANGE
@@ -665,21 +662,21 @@ int writeFile(int sock,char * strfile,char *http_param) {
         if (to==0) { //If no to is specified, it is to the end of the file
             to=size-1;
         }
-        snprintf(a,RBUFFER,"Content-Range: bytes=%d-%d/%d\r\nAccept-Ranges: bytes\r\n",from,to,size);
-
+        snprintf(a,RBUFFER,"Content-Range: bytes=%d-%d/%d\r\nAccept-Ranges: bytes\r\n",from,to,size);        
         lseek(fp,from,SEEK_SET);
         count=to-from+1;
 
         send_http_header_code(sock,206,count,a);
     } else //Normal request
 #endif
-        send_http_header(sock,size,NULL);//Sends header with content length
-
+        {
+            send_http_header(sock,size,NULL);//Sends header with content length
+        }
 
     int reads,wrote;
 
     //Sends file
-    while (count==0 || (reads=read(fp, buf, FILEBUF<count? FILEBUF:count ))!=0) {
+    while (count>0 && (reads=read(fp, buf, FILEBUF<count? FILEBUF:count ))!=0) {
         count-=reads;
         wrote=write(sock,buf,reads);
         if (wrote!=reads) { //Error writing to the socket
