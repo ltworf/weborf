b2cf49d3ad37944fb65f1b20ca644eb18d0e323d
LtWorf <LtWorf@27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a>
2009-10-25 21:14:33 +0000

    added example
    git-svn-id: http://galileo.dmi.unict.it/svn/weborf/trunk@337 27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a

---
+++ examples/index.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+This is an example of index file.
+It adds a "." to a session variable on every refresh, shows the informations and sets a cookie.
+'''
+
+import os
+import sys
+from cgi_weborf import *
+
+cgi.session_start()
+cgi.setcookie("id","33")
+cgi.finalize_headers()
+
+if 'nome' in cgi.SESSION:
+        cgi.SESSION['nome']+="."
+else:
+        cgi.SESSION['nome']="."
+
+sys.stdout.write( "<html>")
+sys.stdout.write( "<body>")
+
+sys.stdout.write( "<h1>example</h1>")
+cgi.pyinfo()
+sys.stdout.write(  "</body>")
+sys.stdout.write(  "</html>")
+
+cgi.savesession()
