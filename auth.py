d6056cc1a730868dd6550b98aac0d6c88a2bd0c9
LtWorf <LtWorf@27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a>
2011-03-11 13:16:22 +0000

    - Deleting debian/files - Changed category in desktop file - Added example auth script on svn
    git-svn-id: http://galileo.dmi.unict.it/svn/weborf/trunk@555 27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a

---
+++ auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Auth script
+# Copyright (C) 2008  Salvo "LtWorf" Tomaselli
+#
+# Relation is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
+
+# This is an example authentication script. You can modify it to make it fit your needs
+import socket,os
+
+s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+try:
+    os.remove("/tmp/weborf_auth.socket")
+except OSError:
+    pass
+s.bind("/tmp/weborf_auth.socket")
+s.listen(1)
+while 1:
+    conn, addr = s.accept()
+    data = conn.recv(4096).split('\r\n')
+    #print data
+    conn.close()
