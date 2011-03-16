f837cf9d0d927049ec27b56c49bfcfc2992457b0
LtWorf <LtWorf@27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a>
2011-03-16 16:02:13 +0000

    - Fixed copyright notice
    git-svn-id: http://galileo.dmi.unict.it/svn/weborf/trunk@576 27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a

--- qweborf/qweborf.py
+++ qweborf/qweborf.py
@@ -3,7 +3,7 @@
 # Weborf
 # Copyright (C) 2010  Salvo "LtWorf" Tomaselli
 # 
-# Relational is free software: you can redistribute it and/or modify
+# Weborf is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
