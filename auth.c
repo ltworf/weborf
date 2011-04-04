30aef363c357ff77c7d8eb18d1bb1a654877cef5
LtWorf <LtWorf@27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a>
2011-04-04 15:33:10 +0000

    - Simplified code for authentication
    git-svn-id: http://galileo.dmi.unict.it/svn/weborf/trunk@583 27c2e2b3-ca6d-4234-b4fe-3aff1b8b8c5a

--- auth.c
+++ auth.c
@@ -37,6 +37,8 @@ extern weborf_configuration_t weborf_conf;
 
 #include "embedded_auth.h"
 
+static int get_username_password(connection_t *connection_prop,char *username, char **password);
+
 /**
 Checks that the authentication socket exists and is a unix socket
 */
@@ -66,6 +68,7 @@ void auth_set_socket(char *u_socket) {
 #endif
 }
 
+
 /**
 This function checks if the authentication can be granted or not calling the external program.
 Returns 0 if authorization is granted.
@@ -74,34 +77,10 @@ int auth_check_request(connection_t *connection_prop) {
     if (weborf_conf.authsock==NULL) return 0;
 
     char username[PWDLIMIT*2];
-    char* password=username; //will be changed if there is a password
+    char *password=username; //will be changed if there is a password
 
-    char* auth=strstr(connection_prop->http_param,"Authorization: Basic ");//Locates the auth information
-    if (auth==NULL) { //No auth informations
-        username[0]=0;
-        //password[0]=0;
-    } else { //Retrieves provided username and password
-        char*auth_end=strstr(auth,"\r\n");//Finds the end of the header
-        if (auth_end==NULL) return -1;
-        char a[PWDLIMIT*2];
-        auth+=21;//Moves the begin of the string to exclude Authorization: Basic
-        if ((auth_end-auth+1)<(PWDLIMIT*2))
-            memcpy(&a,auth,auth_end-auth); //Copies the base64 encoded string to a temp buffer
-        else { //Auth string is too long for the buffer
-#ifdef SERVERDBG
-            syslog(LOG_ERR,"Unable to accept authentication, buffer is too small");
-#endif
-            return ERR_NOMEM;
-        }
-
-        a[auth_end-auth]=0;
-        decode64(username,a);//Decodes the base64 string
-
-        password=strstr(username,":");//Locates the separator :
-        if (password==NULL) return -1;
-        password[0]=0;//Nulls the separator to split username and password
-        password++;//make password point to the beginning of the password
-    }
+    if (get_username_password(connection_prop,username,&password)!=0)
+        return -1;
 
     int result=-1;
 
@@ -113,34 +92,94 @@ int auth_check_request(connection_t *connection_prop) {
                   password,
                   connection_prop->http_param);
 #else
-    {
-        int s,len;
-        struct sockaddr_un remote;
-        s=socket(AF_UNIX,SOCK_STREAM,0);
+    int s,len;
+    FILE *f;
+    
+    struct sockaddr_un remote;
+    s=socket(AF_UNIX,SOCK_STREAM,0);
 
-        remote.sun_family = AF_UNIX;
-        strcpy(remote.sun_path, weborf_conf.authsock);
-        len = strlen(remote.sun_path) + sizeof(remote.sun_family);
-        if (connect(s, (struct sockaddr *)&remote, len) == -1) {//Unable to connect
-            return -1;
-        }
-        char* auth_str=malloc(HEADBUF+PWDLIMIT*2);
-        if (auth_str==NULL) {
-#ifdef SERVERDBG
-            syslog(LOG_CRIT,"Not enough memory to allocate buffers");
-#endif
-            return -1;
-        }
-
-        int auth_str_l=snprintf(auth_str,HEADBUF+PWDLIMIT*2,"%s\r\n%s\r\n%s\r\n%s\r\n%s\r\n%s\r\n",connection_prop->page,connection_prop->ip_addr,connection_prop->method,username,password,connection_prop->http_param);
-        if (write(s,auth_str,auth_str_l)==auth_str_l && read(s,auth_str,1)==0) {//All data written and no output, ok
-            result=0;
-        }
-
-        close(s);
-        free(auth_str);
+    remote.sun_family = AF_UNIX;
+    strcpy(remote.sun_path, weborf_conf.authsock);
+    len = strlen(remote.sun_path) + sizeof(remote.sun_family);
+    if (connect(s, (struct sockaddr *)&remote, len) == -1) {//Unable to connect
+        return -1;
     }
+
+    if ((f=fdopen(s,"r+"))==NULL) {
+        return -1;
+    }
+
+    fprintf(f,
+            "%s\r\n%s\r\n%s\r\n%s\r\n%s\r\n%s\r\n",
+            connection_prop->page,
+            connection_prop->ip_addr,
+            connection_prop->method,
+            username,
+            password,
+            connection_prop->http_param);
+
+    char b;
+    if (fread(&b,1,1,f)==0) {//All data written and no output, ok
+        result=0;
+    }
+
+    fclose(f);
+
 #endif
 
     return result;
+}
+
+/**
+This function returns username and password from an HTTP request
+It has weird parameters and needs to be called like this:
+
+    char username[PWDLIMIT*2];
+    char* password=0;
+
+    get_username_password(connection_prop,username,&password)
+
+the username array will be filled with username and password
+and the password pointer will point to the password
+
+If there is no authentication, it will set username[0]=0 and will
+not change the value of password.
+
+return value: 0 if everything is ok
+
+NOTE: It always assumes size of the array as PWDLIMIT*2
+*/
+static int get_username_password(connection_t *connection_prop,char *username, char **password) {
+
+    char* auth=strstr(connection_prop->http_param,"Authorization: Basic ");//Locates the auth information
+    if (auth==NULL) { //No auth informations
+        username[0]=0;
+        //password[0]=0;
+        return 0;
+    }
+
+    //Retrieves provided username and password
+    char*auth_end=strstr(auth,"\r\n");//Finds the end of the header
+    if (auth_end==NULL) return -1;
+    char a[PWDLIMIT*2];
+    auth+=strlen("Authorization: Basic ");//Moves the begin of the string
+    if ((auth_end-auth+1)<(PWDLIMIT*2))
+        memcpy(&a,auth,auth_end-auth); //Copies the base64 encoded string to a temp buffer
+    else { //Auth string is too long for the buffer
+#ifdef SERVERDBG
+        syslog(LOG_ERR,"Unable to accept authentication, buffer is too small");
+#endif
+        return ERR_NOMEM;
+    }
+
+    a[auth_end-auth]=0;
+    decode64(username,a);//Decodes the base64 string
+
+    (*password)=strstr(username,":");//Locates the separator :
+    if ((*password)==NULL) return -1;
+    (*password[0])=0;//Nulls the separator to split username and password
+    (*password)++;//make password point to the beginning of the password
+
+    return 0;
+
 }
