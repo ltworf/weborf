5b64bd1625b1c9476c6301469b7322609cb0040c
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2015-10-15 16:39:56 +0200

    copied from relational
    script to generate the website, statically

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+for i in pages/*
+do
+    php templates/header > $(basename $i).html
+    php $i >> $(basename $i).html
+    php templates/footer >> $(basename $i).html
+    git add $(basename $i).html
+done
+
+git commit -m "run of compile.sh"
