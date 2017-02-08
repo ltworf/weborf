426b961f2f3c1da0014dbea85ebe9a338c1e6b6f
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2017-02-08 20:32:51 +0100

    Prepare for new version

--- qweborf.setup.py
+++ qweborf.setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    version='0.13.4',
+    version='0.13.5',
     name='qweborf',
     author="Salvo 'LtWorf' Tomaselli",
     author_email='tiposchi@tiscali.it',
