5990c98ff8b3d2af1f305d8e823c930f50883093
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2017-08-05 17:44:41 +0200

    Moved imports in the right position
    No reason to have those imports in the .h file

--- mystring.c
+++ mystring.c
@@ -20,6 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "options.h"
 
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
 #include "mystring.h"
 
 /**
