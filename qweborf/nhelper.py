2e1553d6e188a6c79f04c228a3ec36065516306c
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2018-12-28 11:52:14 +0100

    Make mypy happy

--- qweborf/nhelper.py
+++ qweborf/nhelper.py
@@ -263,7 +263,7 @@ def get_redirections() -> List[Redirection]:
     for i in out:
         m = re.match(
             r'[ ]*([0-9]+)[ ]+(UDP|TCP)[ ]+([0-9]+)->([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):([0-9]+)[ ]+\'(.*)\' \'\'', i)
-        if m != None:
+        if m is not None:
             redirect = m.groups()
             r = Redirection(redirect[4], redirect[
                             2], redirect[3], redirect[1], redirect[5])
