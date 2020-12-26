88c6ed29c9a4cfb1e9d76ffdc0ef68be8f6863b7
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2020-12-26 11:46:06 +0100

    Bump copyright

--- myio.c
+++ myio.c
@@ -1,6 +1,6 @@
 /*
 Weborf
-Copyright (C) 2010-2018  Salvo "LtWorf" Tomaselli
+Copyright (C) 2010-2020  Salvo "LtWorf" Tomaselli
 
 Weborf is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
