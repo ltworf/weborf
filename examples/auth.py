f74efcba28bdd3112befc0d197cd712138a12e51
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2023-03-12 11:59:43 +0100

    Change auth example to be async
    In this way it will not serialise parallel requests.

--- examples/auth.py
+++ examples/auth.py
@@ -2,7 +2,7 @@
 # This is an example authentication script. You can modify it to make it fit your needs
 
 # Weborf
-# Copyright (C) 2019  Salvo "LtWorf" Tomaselli
+# Copyright (C) 2019-2023  Salvo "LtWorf" Tomaselli
 #
 # Weborf is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,8 +19,11 @@
 #
 # author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
 
-import socket,os
+import asyncio
+import os
 from typing import NamedTuple
+from syslog import *
+
 
 class Request(NamedTuple):
     url: bytes
@@ -30,30 +33,53 @@ class Request(NamedTuple):
     password: bytes
     protocol: bytes
 
-s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-try:
-    os.remove("/run/weborf/weborf_auth.socket")
-except OSError:
-    pass
-s.bind("/run/weborf/weborf_auth.socket")
-s.listen(1)
-while True:
-    conn, addr = s.accept()
-    data = conn.recv(4096).split(b'\r\n')
+SOCKET_PATH = "/run/weborf/weborf_auth.socket"
 
-    request = Request(*data[:6])
-    headers = {k:v for k, v in(i.split(b': ', 1) for i in data[6:] if i)}
+async def main():
+    try:
+        os.remove(SOCKET_PATH)
+    except OSError:
+        pass
 
-    # Allow all from internal network
-    if request.addr.startswith(b'::ffff:10.0.'):
-        conn.close()
-        continue
+    l = asyncio.get_running_loop()
+    server = await l.create_unix_server(Proto, SOCKET_PATH)
+    await server.serve_forever()
 
-    # Require a password for some content
-    if request.url.startswith(b'/movies') or request.url.startswith(b'/music'):
-        if request.username != b'user' and request.password != b'pass':
-            conn.send(b' ')
-    # Require another password for all other content
-    elif request.username != b'user2' and request.password != b'pass2':
-        conn.send(b' ')
-    conn.close()
+
+class Proto(asyncio.Protocol):
+    def allow(self):
+        self.transport.close()
+
+    def deny(self):
+        self.transport.write(b' ')
+        self.transport.close()
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data: bytes) -> None:
+        fields = data.split(b'\r\n')
+        request = Request(*fields[:6])
+        headers = {k:v for k, v in(i.split(b': ', 1) for i in fields[6:] if i)}
+
+        # Allow all from internal network
+        if request.addr.startswith(b'::ffff:10.0.'):
+            self.allow()
+            return
+
+        # Require a password for some content
+        if request.url.startswith(b'/movies/') or request.url.startswith(b'/music/'):
+            if request.username != b'user' and request.password != b'pass':
+                self.deny()
+                return
+            else:
+                self.allow()
+                return
+        # Require another password for all other content
+        elif request.username == b'user2' and request.password == b'pass2':
+            self.allow()
+            return
+        self.deny()
+
+
+asyncio.run(main())
